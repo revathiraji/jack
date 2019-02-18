0ae84eaa6f0c2bba22e7948a0f28e83e25b49b46
revathiraji <47025085+revathiraji@users.noreply.github.com>
2019-02-18 15:21:04 +0530

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,10 @@
+num 1=int(input(enter the first num)
+num 2=int(input(enter the second num)
+if(num 1>num 2) &(num 1>num );
+      largest=num1
+ elif (num2 > num1)&(num2 >num3);
+   largest=num 2
+else
+    largest=num 3
+ print("the largest number is"largest)   
+   
